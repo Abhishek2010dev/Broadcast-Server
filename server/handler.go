b699d37f04680c7c14daaceb78835fe9e3fee22a
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-26 22:04:08 +0530

    HandleConnection done

--- server/handler.go
+++ server/handler.go
@@ -1,9 +1,38 @@
 package server
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
 
 func HandleConnections(hub *Hub) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("Upgrade error:", err)
+			return
+		}
 
+		client := &Client{conn: conn}
+		hub.register <- client
+
+		defer func() {
+			hub.unregister <- client
+			client.conn.Close()
+		}()
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Read error:", err)
+				break
+			}
+			hub.broadcast <- message
+
+		}
 	}
 }
