27d5c7993ad0c52721da7ada9ffbf96f5565648b
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-26 21:41:27 +0530

    Hub strcut

--- server/hub.go
+++ server/hub.go
@@ -1,4 +1,31 @@
 package server
 
+import "sync"
+
 type Hub struct {
+	clients    map[*Client]struct{}
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	mutex      sync.RWMutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]struct{}),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mutex.Lock()
+			h.clients[client] = struct{}{}
+			h.mutex.Unlock()
+		}
+	}
 }
